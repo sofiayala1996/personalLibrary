8cbe922afcb36825fbad7291feec59d75d898e57
sofiayala1996 <sofiayala16@gmail.com>
2022-05-17 20:22:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+class Libro{
+    constructor(titulo, autor, ISBN) {
+        this.titulo = titulo;
+        this.autor   = autor;
+        this.ISBN = ISBN
+    }
+    mostrarLibro() {
+        return `Titulo: ${this.titulo} Autor: ${this.autor} ISBN: ${this.ISBN}`
+    }
+}
+
+const Libros = [ ];
+
+//Almacenar libros en localStorage
+const guardarLocal = (clave, valor) => {localStorage.setItem(clave, valor) }; 
+if(
+    ! localStorage.getItem("librosAlmacenados")
+){
+    guardarLocal("librosAlmacenados", JSON.stringify(Libros));
+}
+
+//Ingresar libro nuevo
+document.querySelector("#btn").addEventListener('click', () => {
+    agregarLibro();
+    $('#exampleModalCenter').modal('hide');
+})
+
+//API
+async function f1(isbn){
+    return fetch(`https://api.nytimes.com/svc/books/v3/reviews.json?isbn=${isbn}&api-key=hcdoOv4di8KDwLGjGi5me0trcc0fq0R5`)
+    .then(response => response.json())
+    .then(data => data.results[0].summary);
+}
+
+async function mostrandoLibros() {
+    const save = localStorage.getItem("librosAlmacenados");
+    const change = JSON.parse(save);
+    const list = document.querySelector('#book-list');
+    list.innerHTML = ""
+    for (const libro of change) {
+        const variable = await f1(libro.ISBN);
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td><img src = "https://covers.openlibrary.org/b/isbn/${libro.ISBN}-S.jpg"></td>
+        <td>${libro.titulo}</td>
+        <td>${libro.autor}</td>
+        <td>${libro.ISBN}</td>
+        <td>${variable}</td>
+        <td><button type="button" id="boton-5" class="btn btn-danger delete ">X</button></td>`;
+    list.appendChild(row);
+    }
+    
+}
+
+
+//Eliminar un libro agregado
+function deleteBook(el) {
+    if(el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    deleteBook(e.target);})
+    
+function removeBook(isbn) {
+        const books = localStorage.getBooks();
+    
+        books.forEach((book, index) => {
+          if(book.isbn === isbn) {
+            books.splice(index, 1);
+          }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+
+function agregarLibro(){
+    //Obtener elementos del formulario
+    let titulo = document.getElementById("inputTitulo").value;
+    let autor = document.getElementById("inputAutor").value;
+    let ISBN = document.getElementById("inputISBN").value;
+    //Sumarlos al array Libros
+    Libros.push(new Libro(titulo, autor, ISBN));
+    //Actualizar la 'base de datos'
+    const actualizar = (clave, valor) => {localStorage.setItem(clave, valor) }; 
+    actualizar("librosAlmacenados", JSON.stringify(Libros));
+mostrandoLibros();
+}
+
+
